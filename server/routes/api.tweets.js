704d759642140fab386565584ea2b4a8e443e1cb
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-16 12:32:11 +0700

    added tweet routes

---
+++ server/routes/api.tweets.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('controllers/api.tweets')
+/*
+  * recent tweet (maximal 50 tweets)
+  * controller's function : getRecentTweets
+*/
+router.get('/', controller.getRecentTweets);
+
+/*
+  * process new tweet
+  * controller's function : postNewTweet
+*/
+router.get('/', controller.postNewTweet);
+
+/*
+  * delete a tweet
+  * controller's function : deleteOneTweet
+*/
+router.delete('/:id', controller.deleteOneTweet);
+
+/*
+  * seed data into tweets collection
+  * controller's function : seedDataTweets
+*/
+router.post('/', controller.seedDataTweets);
+
+/*
+  * delete all data in tweets collection
+  * controller's function : deleteAllTweets
+*/
+router.delete('/', controller.deleteAllTweets);
+
+/*
+  * Search a tweet by hashtag
+  * controller's function : searchTweetByHashTag
+*/
+router.get('/:hashtag', controller.searchTweetByHashTag);
+
+module.exports = router;
