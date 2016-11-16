7e7e9b232e7485155c09a1796324e89c82541470
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-16 11:35:55 +0700

    create controller structure for twit

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/api/twit', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
---
+++ server/controllers/controllers.api.twit.js
@@ -0,0 +1,31 @@
+const Twit = require('../models/twit')
+
+module.exports = {
+    twitSeed: (req, res) => {
+
+    },
+
+    getTwitRecent: (req, res) => {
+
+    },
+
+    getTwitById: (req, res) => {
+
+    },
+
+    getTwitByHashtag: (req, res) => {
+
+    },
+
+    createTwit: (req, res) => {
+
+    },
+
+    deleteAllTwit: (req, res) => {
+
+    },
+
+    deleteTwitById: (req, res) => {
+
+    }
+}
