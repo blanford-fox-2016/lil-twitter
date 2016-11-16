77aba8c22ee3093580344e1340c174d04fa7291b
broerjuang <juangwiantoro@gmail.com>
2016-11-16 11:44:12 +0700

    crating tweet routes from given api routes

---
+++ server/routes/tweet.route.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// express dipendencies
+const express = require('express');
+const routes = express.Router();
+
+// local dipendencies
+const Controller = require('../controllers/tweet.controller');
+
+// GET all recent tweeet
+router.get('/tweets/recents', Controller.list)
+
+// Get all tweet by hastag
+router.get('/tweets/search/:hastag', Controller.findHastag)
+
+// POST a new tweet
+router.post('/tweets', Controller.generate)
+
+// Get the most popular tweet
+router.get('/tweets/hastags/popular', Controller.findPopular)
+
+module.exports = router;
