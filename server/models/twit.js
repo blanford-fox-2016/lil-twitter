b2250e264e2d95f66a6394a6f9fc1405d2b9e174
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-16 11:27:18 +0700

    install dotenv

---
+++ server/models/twit.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Twit = new Schema({
+    avatar_url: {
+        type: String
+    },
+    content: {
+        type: String
+    },
+    createdAt: {
+        type: String
+    },
+    updatedAt: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    hashtag_names: []
+})
+
+module.exports = mongoose.model('Twit', Twit)
