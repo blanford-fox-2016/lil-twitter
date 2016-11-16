2b2a483a78b7cc61be149475c9cf8de8f9d554c4
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-16 15:41:36 +0700

    user's server side API

---
+++ server/controllers/api.users.js
@@ -0,0 +1,168 @@
+'use strict'
+const passport = require('pasport')
+const User = require('../models/api.users')
+const jwt = require('jsonwebtoken')
+
+/*
+  * @api {get} /api/users
+  * @api purpose get all users
+  * @apiName allUsers
+  * @apiGroup users
+  *
+  * @apiSuccess show all user's username {String}
+*/
+let allUsers = (req, res) => {
+  User.find({}, (err, users) => {
+    if(err) res.status(400).json({'error': `Error: ${err}`})
+    if(!users) res.status(404).json({'message': 'Failed to get all users'})
+    console.log(`get all users`);
+    res.status(200).json(users)
+  })
+}
+
+/*
+  * @api {post} /api/users
+  * @api purpose post new user
+  * @apiName addUser
+  * @apiGroup users
+  *
+  * @apiSuccess add new user's username {String}
+*/
+let addUser = (req, res, next) => {
+  console.log(`ini masuk`);
+  console.log(req.body);
+  User.register({
+    userId   : 1,
+    username : req.body.username
+    // arcticle id add here
+  }, req.body.password, (err, new_user) => {
+    console.log(`test`);
+    if(err) res.status(400).json({'error': 'Error: ${err}'})
+    if(!new_user) res.status(404).json({'message': 'Failed to add new user'})
+
+    passport.authenticate('local')(req, res, () => {
+        if (err) {
+          return next(err)
+        }else{
+          res.status(200).json(new_user)// nanti jwt di lempar
+        }
+
+    })
+  })
+}
+
+/*
+  * @api {put} /api/users/:id
+  * @api purpose put a user
+  * @apiName editUser
+  * @apiGroup users
+  *
+  * @apiSuccess edit a user
+*/
+let editUser = (req, res) => {
+  User.findOneAndUpdate({
+    userId : req.params.id
+  }, req.body, {
+    new: true
+  }, (err, updated_user) => {
+    if(err) res.status(400).json({'error': 'Error: ${err}'})
+    if(!updated_user) res.status(404).json({'message': 'Failed to update user'})
+
+    res.status(200).json(updated_user)
+  })
+}
+
+/*
+  * @api {delete} /api/users/:id
+  * @api purpose delete a user
+  * @apiName deleteUser
+  * @apiGroup users
+  *
+  * @apiSuccess delete a user
+*/
+let deleteUser = (req, res) => {
+  console.log(`params: ${req.params.id}`);
+  User.findOneAndRemove({
+    userId : req.params.id
+  }, (err, deleted_user) => {
+    if(err) res.status(400).json({'error': 'Error: ${err}'})
+    if(!deleted_user) res.status(404).json({'message': 'Failed to delete user'})
+    console.log(deleted_user);
+    res.status(200).json(deleted_user)
+  })
+}
+
+/*
+  * @api {POST} /api/users/register_local
+  * @api purpose to register a user
+  * @apiName registerLocalUser
+  * @apiGroup users
+  *
+  * @apiSuccess register a user
+*/
+let registerLocalUser = (req, res, next) => {
+  console.log(`register`);
+  console.log(req.body);
+
+  User.register(new User({
+    username : req.body.username
+  }),
+  req.body.password,
+  (err, new_user) => {
+    if(err) res.status(400).json({'error': `Register Error: ${err}`})
+    if(!new_user) res.status(404).json({'message': 'Failed to register a user'})
+
+    // passport.authenticate('local', {
+    //   successRedirect: '/',
+    //   successFlash: true,
+    //   failureRedirect: '/register',
+    //   failureFlash: true
+    // }, (err, user, info) => {
+    //   if(err) return res.status(400).json({'error': 'Login Error: ${err}'})
+    //   if(!user) return res.status(404).json({'message': 'Register succeded but sign in falied'})
+    //
+    //   return res.status(200).json({
+    //     token: jwt.sign({
+    //       sub: user._id,
+    //       username: user.username
+    //     }, 'secret')
+    //   })
+    // })(req, res, next)
+  })
+}
+
+/*
+  * @api {POST} /api/users/login
+  * @api purpose to login a user
+  * @apiName apiSuccess
+  * @apiGroup users
+  *
+  * @apiSuccess login a user
+*/
+let loginUser = (req, res, next) => {
+  passport.authenticate('local',
+  {},
+  (err, user, info) => {
+      console.log(user)
+      if(err){
+        return res.json(err)
+      }else {
+        return res.status(200).json({
+          token: jwt.sign({
+            sub: user._id,
+            username: user.username
+          }, 'secret')
+        })
+      }
+
+  })(req, res, next)
+}
+
+module.exports = {
+  allUsers   : allUsers,
+  addUser    : addUser,
+  editUser   : editUser,
+  deleteUser : deleteUser,
+  registerLocalUser : registerLocalUser,
+  loginUser  : loginUser
+}
--- server/routes/api.users.js
+++ server/routes/api.users.js
@@ -2,40 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('../controllers/api.users')
-// /*
-//   * recent tweet (maximal 50 tweets)
-//   * controller's function : getRecentTweets
-// */
-// router.get('/', controller.getRecentTweets);
-//
-// /*
-//   * process new tweet
-//   * controller's function : postNewTweet
-// */
-// router.post('/', controller.postNewTweet);
-//
-// /*
-//   * delete a tweet
-//   * controller's function : deleteOneTweet
-// */
-// router.delete('/:id', controller.deleteOneTweet);
-//
-// /*
-//   * seed data into tweets collection
-//   * controller's function : seedDataTweets
-// */
-// router.post('/seed', controller.seedDataTweets);
-//
-// /*
-//   * delete all data in tweets collection
-//   * controller's function : deleteAllTweets
-// */
-// router.delete('/', controller.deleteAllTweets);
-//
-// /*
-//   * Search a tweet by hashtag
-//   * controller's function : searchTweetByHashTag
-// */
-// router.get('/search', controller.searchTweetByHashTag);
+
+/* GET All Users. */
+router.get('/', controller.allUsers)
+
+/* Process New User. */
+router.post('/', controller.addUser);
+
+/* Process Register Local User. */
+router.post('/register_local', controller.registerLocalUser);
+
+/* Process Login User. */
+router.post('/login', controller.loginUser);
+
+/* Process Edit a User. */
+router.put('/:id', controller.editUser);
+
+/* Process Delete a User. */
+router.delete('/:id', controller.deleteUser);
 
 module.exports = router;
