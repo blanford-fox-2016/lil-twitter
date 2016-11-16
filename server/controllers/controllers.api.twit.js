9126658b5ceb762bab5ce4911a39f82a708c95ce
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-16 12:03:13 +0700

    seed twit from postman works

--- server/controllers/controllers.api.twit.js
+++ server/controllers/controllers.api.twit.js
@@ -2,7 +2,38 @@ const Twit = require('../models/twit')
 
 module.exports = {
     twitSeed: (req, res) => {
+        const twit = [
+            {
+                avatar_url: 'http://www.littlestarsplayschool.com/images/1466407606547378dummy.png',
+                content: 'content 1',
+                username: 'andi'
+            },
+            {
+                avatar_url: 'http://www.littlestarsplayschool.com/images/1466407606547378dummy.png',
+                content: 'content 2',
+                username: 'budi'
+            },
+            {
+                avatar_url: 'http://www.littlestarsplayschool.com/images/1466407606547378dummy.png',
+                content: 'content 3',
+                username: 'cindy'
+            },
+            {
+                avatar_url: 'http://www.littlestarsplayschool.com/images/1466407606547378dummy.png',
+                content: 'content 4',
+                username: 'doni'
+            },
+            {
+                avatar_url: 'http://www.littlestarsplayschool.com/images/1466407606547378dummy.png',
+                content: 'content 5',
+                username: 'eri'
+            }
+        ]
 
+        Twit.create(twit, (err, data) => {
+            if (err) res.json(err)
+            else res.json(data)
+        })
     },
 
     getTwitRecent: (req, res) => {
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const twitController = require('../controllers/controllers.api.twit')
 
+router.get('/seed', twitController.twitSeed)
 router.post('/', twitController.createTwit)
 
 module.exports = router;
